c68d863a34968d1b6157e6718e556066d6be3ec0
Champagnest <mchampagned@protonmail.com>
2021-11-25 11:51:37 -0800

    minimal changes, pushing to switch branches

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) { 
   event.preventDefault(); 
+  var taskNameInput = document.querySelector("input[name='task-name']");
 
   var listItemEl = document.createElement("li"); 
   listItemEl.className = "task-item"; 
